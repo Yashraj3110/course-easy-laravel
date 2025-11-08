f920eb0386d2e092f3ab2f4318420fdb852f17f8
Yashraj3110 <3110yashraj@gmail.com>
2025-11-08 22:31:22 +0530

    done

--- api/index.php
+++ api/index.php
@@ -1,2 +1,18 @@
 <?php
-require __DIR__ . "/../public/index.php";
+
+// Set Laravel base path
+define('LARAVEL_START', microtime(true));
+$basePath = dirname(__DIR__); // points to project root
+
+require $basePath . '/vendor/autoload.php';
+$app = require_once $basePath . '/bootstrap/app.php';
+
+// Run the kernel
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+
+$response->send();
+$kernel->terminate($request, $response);
